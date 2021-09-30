e0d649da682aa18c79e37563723fd0e7d6968187
Tobias Anzinger <Tobias.Anzinger@jambit.com>
2021-09-30 12:13:41 +0200

    add Jdbc 03 Execute Querry (skript page 13) and refactor

---
+++ src/main/java/javaDatabaseConnectivity/Jdbc03ExecuteQuery.java
@@ -0,0 +1,50 @@
+package javaDatabaseConnectivity;
+
+import java.sql.*;
+
+/**
+ * Beispiel: Jdbc03ExecuteQuery
+ * 
+ * @author <a href="mailto:mgn.schule@gmx.de">Michael Niedermair</a>
+ * @version $Revision: 1117 $
+ */
+public class Jdbc03ExecuteQuery {
+
+   public static void main(final String[] args) {
+
+      System.out.println("JDBC Execute Query");
+      System.out.println("==================");
+
+      Connection connection = null;
+      Statement statement = null;
+
+      try {
+
+         // 1. Holen einer Connection
+         connection = Util.getConnection("db");
+
+         // 2. Erzeugen eines Statements
+         statement = connection.createStatement();
+
+         // 3. Struktur der Tabelle LIEFERANT anzeigen
+         String sql1 = "DESC lieferant";
+         ResultSet rs = statement.executeQuery(sql1);
+
+         // 4. Metadatern ermitteln
+         ResultSetMetaData rsmd = rs.getMetaData();
+         int colmax = rsmd.getColumnCount();
+         // TODO ...
+         System.out.println(rsmd);
+         System.out.println(colmax);
+         // 5. Resultset ausgeben
+         Util.printResultSet(rs);
+
+      } catch (SQLException e) {
+         System.out.println(e.getMessage());
+         e.printStackTrace();
+      } finally {
+         Util.close(statement);
+         Util.close(connection);
+      }
+   }
+}
